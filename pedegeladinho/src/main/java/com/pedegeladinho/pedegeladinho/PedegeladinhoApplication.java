e6c85a7639b1225fd895a8924c3f808d4606bf7e
Fernando <pleasecodebro@gmail.com>
2019-08-05 17:20:07 -0300

    adicionando servidor ao git

---
+++ pedegeladinho/src/main/java/com/pedegeladinho/pedegeladinho/PedegeladinhoApplication.java
@@ -0,0 +1,13 @@
+package com.pedegeladinho.pedegeladinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedegeladinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedegeladinhoApplication.class, args);
+	}
+
+}
